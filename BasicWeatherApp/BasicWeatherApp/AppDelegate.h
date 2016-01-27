68a8f3cd76c798833c7a847598da733f778afb41
Anne Lindsley <annelindsley@terk.local>
2016-01-26 21:44:48 -0800

    created new project

---
+++ BasicWeatherApp/BasicWeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicWeatherApp
+//
+//  Created by Anne Lindsley on 1/26/16.
+//  Copyright © 2016 Anne Lindsley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicWeatherApp/BasicWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicWeatherApp
+//
+//  Created by Anne Lindsley on 1/26/16.
+//  Copyright © 2016 Anne Lindsley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
